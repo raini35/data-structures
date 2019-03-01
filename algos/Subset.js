76ea87505bbb55f678a21cf4b684ee254273025e
Rainier G <rainier.f.go@gmail.com>
2019-02-28 23:56:02 -0600

    Added notes to Subset.js

--- algos/Subset.js
+++ algos/Subset.js
@@ -1,3 +1,4 @@
+// Backtracking problem
 function getSubsets(arr) {
   let output = [];
   recurseSubset(arr, [], output, 0);
@@ -6,11 +7,16 @@ function getSubsets(arr) {
 
 function recurseSubset(arr, subset, output, index) {
   output.push(subset);
+
+  // This if statement is necessary to stop the function from going in an 
+  // infinite loop
   if(index == arr.length) {
     return;
   }
 
   for(var i = index; i < arr.length; i++) {
+    // this backtracks because the subset stays the same for every iteration
+    // the argument for subset in the next call of recurseSubset gets changed
    recurseSubset(arr, [...subset, arr[i]], output, i + 1);
   }
 }
